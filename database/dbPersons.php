d7c8cb2d6b0232051f735b077b645e188a12d26f
Gustav Goldman <jgvots2@gmail.com>
2024-03-16 16:19:23 -0400

    touched up a query line

--- database/dbPersons.php
+++ database/dbPersons.php
@@ -107,13 +107,14 @@ function remove_person($id) {
 function retrieve_id($first_name, $last_name, $phone) {
     $persons = array();
     $con=connect();
-    $query = 'SELECT id FROM dbPersons WHERE phone1 = "' . $phone . '" AND first_name = "' . $first_name . '" AND last_name = "' . $last_name . '"';
+    $query = 'SELECT id FROM dbPersons WHERE phone1 = ' . $phone . ' AND first_name = ' . $first_name . ' AND last_name = ' . $last_name . '';
     $result = mysqli_query($con,$query);
     while ($result_row = mysqli_fetch_assoc($result)) {
         $the_person = make_a_person($result_row);
         $persons[] = $the_person;
     }
     return $persons;
+    
 }
 
 /*
