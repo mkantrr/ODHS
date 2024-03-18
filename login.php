c1473ea1515fd828772a89ab25d81e1039a2541b
ByronWill <102559985+ByronWill@users.noreply.github.com>
2024-03-18 19:44:51 -0400

    fixed  access level comparison within login.php

--- login.php
+++ login.php
@@ -11,7 +11,7 @@
 
     // redirect to index if already logged in
     if (isset($_SESSION['_id'])) {
-        if ($_SESSION['access_level'] = 1){
+        if ($_SESSION['access_level'] == 1){
             header('Location: vms_index.php');
         } else if ($_SESSION['access_level'] > 1){
             header('Location: centralMenu.php');
