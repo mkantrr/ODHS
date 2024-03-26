fbf8576e187e175bcf479e72157ea014774285e5
ByronWill <102559985+ByronWill@users.noreply.github.com>
2024-03-26 12:21:27 -0400

    Set header for vmsroot to central_menu.php once logged in

--- login.php
+++ login.php
@@ -57,6 +57,7 @@
                 // hard code root privileges
                 if ($user->get_id() == 'vmsroot') {
                     $_SESSION['access_level'] = 3;
+                    header('Location:centralMenu.php');
                 }
                 if ($changePassword) {
                     $_SESSION['access_level'] = 0;
