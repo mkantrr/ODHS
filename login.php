26e28326f5216e97d6b2ed7fb52c9d77b0493e57
ByronWill <102559985+ByronWill@users.noreply.github.com>
2024-04-01 14:22:02 -0400

    Testing modification to redirect if already logged in portion of login.php

--- login.php
+++ login.php
@@ -11,12 +11,10 @@
 
     // redirect to index if already logged in
     if (isset($_SESSION['_id'])) {
-        if($SESSION['access_level'] == 0){
-            header('Location: login.php');
-        } elseif ($_SESSION['access_level'] == 1) {
+        if($SESSION['access_level'] > 1){
+            header('Location: centralMenu.php');
+        } else if ($_SESSION['access_level'] == 1) {
             header('Location:vms_index.php');
-        } else{
-            header('Location:centralMenu.php');
         }
         die();
     }
