53a5b45ecc10d150b58fa0af008a2ec3148db5f8
ByronWill <102559985+ByronWill@users.noreply.github.com>
2024-04-15 15:17:28 -0400

    added a modified version of the forgot email functionality from login.php into logHoursForm.php

--- logHoursForm.php
+++ logHoursForm.php
@@ -56,4 +56,8 @@ function buildSelect($name, $disabled=false, $selected=null) {
     <?php if ($loggedIn): ?>
         <a class="button cancel" href="index.php" style="margin-top: .5rem">Cancel</a>
     <?php endif ?>
+    <p><a href="#" onclick="showMessage()">Forgot email address for your account?</a></p>
+            <div id="forgotMessage" style="display: none;">
+                <p>Please contact your administrator for assistance with your email address.</p>
+            </div>
 </main>
