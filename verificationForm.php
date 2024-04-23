3dcd27c864215257078770b860b3c7c981a8154b
Gustav Goldman <jgvots2@gmail.com>
2024-04-23 16:28:17 -0400

    Added the verification form pdf

--- verificationForm.php
+++ verificationForm.php
@@ -31,7 +31,7 @@ $pdf->AddPage();
 $pdf->Image('images/odhs.png',50,8,100); 
           
 // Set font family to Arial bold  
-$pdf->SetFont('Arial','',20); 
+$pdf->SetFont('Arial','',12); 
   
 // Move to the right 
 $pdf->Cell(80); 
@@ -39,11 +39,42 @@ $pdf->Cell(80);
 // Line break 
 $pdf->Ln(40); 
 
+//First Paragraph
 $pdf->Cell(40,10,'Old Dominion Humane Society');
-$pdf->Ln(10); 
+$pdf->Ln(5); 
 $pdf->Cell(40,10,'3602 Lafayette Blvd. Ste. 102');
-$pdf->Ln(10); 
+$pdf->Ln(5); 
 $pdf->Cell(40,10,'Fredericksburg, VA 22408');
+$pdf->Ln(5);
+$pdf->Cell(40,10,'To Whom It May Concern:');
+$pdf->Ln(10);
+$pdf->Cell(40,10,'Old Dominion Humane Society is a volunteer organization dedicated to providing a better life');
+$pdf->Ln(5);
+$pdf->Cell(40,10, 'for dogs through adoption and education as well as to assist in the effort to limit the');
+$pdf->Ln(5);
+$pdf->Cell(40,10,'overpopulation of these animals through a spay and neuter program.');
+$pdf->Ln(10);
+
+//Second Paragraph
+$pdf->Cell(40,10,'[FIRST NAME LAST NAME] self-reports that he volunteered for [NUMBER] hours between');
+$pdf->Ln(5);
+$pdf->Cell(40,10,'[OLDEST DATE LOGGED] and [MOST RECENT DATE LOGGED] with our rescue completing several');
+$pdf->Ln(5);
+$pdf->Cell(40,10,'tasks which include the following: walking dogs, cleaning kennels, cleaning dishes, washing and');
+$pdf->Ln(5);
+$pdf->Cell(40,10,'folding laundry, and socializing with the dogs. [PERSON] has contributed wonderfully to our');
+$pdf->Ln(5);
+$pdf->Cell(40,10,'organization.');
+$pdf->Ln(10);
+
+//Table of Hours
+$pdf->Cell(40,10,"The following is a record of [PERSON]'s self-reported hours:");
+$pdf->Ln(10);
+
+//Contact
+$pdf->Cell(40,10,"If you have any questions please contact us at volunteer@olddominionhumanesociety.org for");
+$pdf->Ln(5);
+$pdf->Cell(40,10,"additional information.");
 $pdf->Output(); 
   
 ?>
